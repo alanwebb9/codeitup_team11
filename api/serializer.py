afd599e3bcc4c20e14fb8baa8f49929d084c21dc
dkmdebugin <salimonjamiu@yahoo.com>
2020-02-19 18:19:29 +0000

    Create New Endpoint

--- api/serializer.py
+++ api/serializer.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 
 from api.models import (User, WMData,
-                    Windmills)
+                    Windmills, Game_WMill_Asset)
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -17,3 +17,8 @@ class WMDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = WMData
         fields = ("id", "speed", "height", "year")
+
+class Game_WMill_AssetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Game_WMill_Asset
+        fields = ("county_id", "county_name", "WMill_Height", "WMill_Speed", "WMill_Power", "WMill_Coins")
